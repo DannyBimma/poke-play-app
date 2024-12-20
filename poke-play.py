54a0d51abdca2ebe4ca76e289259560aec2b233e
DannyBimma <dantrotman92@gmail.com>
2024-12-20 18:25:24 -0400

    define main func, prototype core functions and logic

--- poke-play.py
+++ poke-play.py
@@ -4,9 +4,28 @@ from typing import List, Optional
 import sys
 from dataclasses import dataclass
 
-"""Class to hold Pokemon data"""
+# Data struct for the Pokes
 @dataclass
 class Pokemon:
     name: str
     id: int
     types: List[str]
+
+
+def main():
+    api = PokedexAPI()
+
+    if usr_name == "DannyBimma":
+        booster_pck = api.get_pokes(5, mew=True)
+    else:
+        booster_pck = api.get_pokes(5)
+    
+    if pck:
+        display_pck(pck)
+        return 0
+    else:
+        print("⛔️: Your Booster Pack was a dud, bud!! Do try again 🥹", file=sys.stderr)
+        return 1
+
+if __name__ == "__main__":
+    sys.exit(main())
